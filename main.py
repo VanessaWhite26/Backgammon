05f6d9e4f5839d3cf14a6ee09385a39c79bd6781
VanessaWhite26 <46722056+VanessaWhite26@users.noreply.github.com>
2020-04-11 20:24:03 -0700

    Add functionality to the sxit button

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.uix.button import Button
 import kivy
+
 kivy.require("1.9.1")
 
 
@@ -15,7 +16,7 @@ class Background(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        #create textures
+        # create textures
         self.dice_texture = Image(source="pieces.png").texture
         self.dice_texture.wrap = 'repeat'
         self.dice_texture.uvsize = (Window.width / self.dice_texture.width, -1)
@@ -24,20 +25,21 @@ class Background(Widget):
         self.points_texture.wrap = 'repeat'
         self.points_texture.uvsize = (Window.width / self.dice_texture.width, -1)
 
-
-
-
     def scroll_textures(self, time_passed):
-        #update the uvpos
-        self.dice_texture.uvpos = ((self.dice_texture.uvpos[0] + time_passed)%Window.width, self.dice_texture.uvpos[1])
-        self.points_texture.uvpos = ((self.points_texture.uvpos[0] - time_passed) % Window.width, self.points_texture.uvpos[1])
-        #Redraw the texture
+        # update the uvpos
+        self.dice_texture.uvpos = (
+        (self.dice_texture.uvpos[0] + time_passed) % Window.width, self.dice_texture.uvpos[1])
+        self.points_texture.uvpos = (
+        (self.points_texture.uvpos[0] - time_passed) % Window.width, self.points_texture.uvpos[1])
+        # Redraw the texture
         texture = self.property("dice_texture")
         texture.dispatch(self)
 
         texture = self.property("points_texture")
         texture.dispatch(self)
+
     pass
+
 
 from kivy.clock import Clock
 
@@ -49,55 +51,58 @@ class ButtonApp(App):
         start = Widget()
 
         startButton = Button(text="Start",
-                     font_size="20sp",
-                     background_color=(1, 1, 1, 1),
-                     color=(1, 1, 1, 1),
-                     size=(100,75),
-                     size_hint=(.2, .2),
-                     pos=(Window.width/2 - 50 , Window.height/2 + 50))
+                             font_size="20sp",
+                             background_color=(1, 1, 1, 1),
+                             color=(1, 1, 1, 1),
+                             size=(100, 75),
+                             size_hint=(.2, .2),
+                             pos=(Window.width / 2 - 50, Window.height / 2 + 50))
 
         helpButton = Button(text="Help",
-                     font_size="20sp",
-                     background_color=(1, 1, 1, 1),
-                     color=(1, 1, 1, 1),
-                     size=(100, 75),
-                     size_hint=(.2, .2),
-                     pos=(Window.width/2 - 50 , Window.height/2 -50 ))
-
+                            font_size="20sp",
+                            background_color=(1, 1, 1, 1),
+                            color=(1, 1, 1, 1),
+                            size=(100, 75),
+                            size_hint=(.2, .2),
+                            pos=(Window.width / 2 - 50, Window.height / 2 - 50))
 
         exitButton = Button(text="Exit",
-                     font_size="20sp",
-                     background_color=(1, 1, 1, 1),
-                     color=(1, 1, 1, 1),
-                     size=(100, 75),
-                     size_hint=(.2, .2),
-                     pos=(Window.width/2 - 50 , Window.height/2 - 150))
+                            font_size="20sp",
+                            background_color=(1, 1, 1, 1),
+                            color=(1, 1, 1, 1),
+                            size=(100, 75),
+                            size_hint=(.2, .2),
+                            pos=(Window.width / 2 - 50, Window.height / 2 - 150))
 
         start.add_widget(startButton)
         start.add_widget(helpButton)
         start.add_widget(exitButton)
 
-
         # bind() use to bind the button to function callback
-        # startButton.bind(on_press = self.callback)
-        # helpButton.bind(on_press = self.callback)
-        # exitButton.bind(on_press = self.callback)
+        startButton.bind(on_press=self.startGame)
+        helpButton.bind(on_press=self.helpPage)
+        exitButton.bind(on_press=self.exitGame)
 
         return start
 
-
     # # callback function tells when button pressed
-    # def callback(self, event):
-    #     print("button pressed")
-    #     print('Yoooo !!!!!!!!!!!')
+    def startGame(self, event):
+        print("You started the game!")
+
+    def helpPage(self, event):
+        print("Heading to the help page")
+
+    def exitGame(self, event):
+        exit()
 
 
 
 class MainApp(App):
     def on_start(self):
-        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/500.)
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1 / 500.)
         root = ButtonApp()
         root.run()
+
     pass
 
 
