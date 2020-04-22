f428f612838061f961042e8230f8754214577e49
REX GODBOUT <rexgodbout@gmail.com>
2020-04-22 15:23:00 -0400

    Lets go!!!!!!!!!! I figured it out!!

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import kivy
 
 kivy.require('1.9.0')
-
-
+from kivy.clock import Clock
 from kivy.app import App
 from kivy.core.audio import SoundLoader
 from kivy.lang import Builder
@@ -12,30 +11,13 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
 from kivy.core.window import Window
-from kivy.uix.button import Button
-import kivy
-from kivy.clock import Clock
 
 # You can create your kv code in the Python file
 Builder.load_string("""
 
-# 
-# Background: 
-#     id: background
-#     canvas.before:
-#         Rectangle:
-#             size: self.size
-#             pos: self.pos
-#             source: "backgroundMenu.png"
-#         Rectangle:
-#             size: self.width, 138
-#             pos: self.pos[0], self.pos[1] + self.height -138
-#             texture: self.dice_texture
-#         Rectangle:
-#             size: self.width, 500
-#             pos: self.pos[0], self.pos[1]
-#             texture: self.points_texture
 
+
+         
 
 <Button>:
     font_name: 'Arial'
@@ -45,11 +27,29 @@ Builder.load_string("""
     size_hint: 0.3,0.2
     background_normal: 'leather.png'
     background_down: 'downleather.png'
-    
-
-
 
 <ScreenMenu>:
+
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -400
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "backgroundMenu.png"
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -400
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.width, 500
+                    pos: self.pos[0], self.pos[1]
+                    texture: self.points_texture
     FloatLayout:
         BoxLayout:
             orientation: "vertical"
@@ -57,45 +57,47 @@ Builder.load_string("""
                 Button:
                     pos_hint:{"top": 1,"left": 1}
                     text: "Need Help?"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
                         root.manager.transition.direction = 'right'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_help'
-                        
-                
-     
                 Button:
                     pos_hint:{"top": 1,"right": 1}
                     text: "Play Backgammon?"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
                         root.manager.transition.direction = 'left'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_game'
-                    
+
                 Button:
                     pos_hint:{"top": 1,"right": 2}
                     text: "Scores"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
                         root.manager.transition.direction = 'up'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_scores'
-                
+
                 Button:
                     pos_hint:{"top": 1,"left": 2}
                     text: "QUIT?"
-                    
+
                     on_press:
                         exit()
-        
+    
+    
+    
+    
+
+                
 
 <ScreenGame>:
     BoxLayout:
@@ -108,7 +110,7 @@ Builder.load_string("""
             text: "Quit"
             on_press:
                 exit()
-                
+
 <ScreenHelp>:
     BoxLayout:
         Button:
@@ -116,9 +118,9 @@ Builder.load_string("""
             on_press:
                 root.manager.transition.direction = 'left'
                 root.manager.current = 'screen_menu'        
-                
-                
-                
+
+
+
 <ScreenScores>:
     BoxLayout:
         Button:
@@ -127,51 +129,55 @@ Builder.load_string("""
                 root.manager.transition.direction = 'down'
                 root.manager.current = 'screen_menu'             
 """)
+
+
 class Background(Widget):
     dice_texture = ObjectProperty(None)
     points_texture = ObjectProperty(None)
 
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+    #
         # create textures
         self.dice_texture = Image(source="pieces.png").texture
         self.dice_texture.wrap = 'repeat'
         self.dice_texture.uvsize = (Window.width / self.dice_texture.width, -1)
-
+    #
         self.points_texture = Image(source="points.png").texture
         self.points_texture.wrap = 'repeat'
         self.points_texture.uvsize = (Window.width / self.dice_texture.width, -1)
-
-
-    def scroll_textures(self, time_passed):
-        # update the uvpos
-        self.dice_texture.uvpos = (
-            (self.dice_texture.uvpos[0] + time_passed) % Window.width, self.dice_texture.uvpos[1])
-        self.points_texture.uvpos = (
-            (self.points_texture.uvpos[0] - time_passed) % Window.width, self.points_texture.uvpos[1])
-        # Redraw the texture
-        texture = self.property("dice_texture")
-        texture.dispatch(self)
-
-        texture = self.property("points_texture")
-        texture.dispatch(self)
-
-
+    #
+    # def scroll_textures(self, time_passed):
+    #     # update the uvpos
+    #     self.dice_texture.uvpos = (
+    #         (self.dice_texture.uvpos[0] + time_passed) % Window.width, self.dice_texture.uvpos[1])
+    #     self.points_texture.uvpos = (
+    #         (self.points_texture.uvpos[0] - time_passed) % Window.width, self.points_texture.uvpos[1])
+    #     # Redraw the texture
+    #     texture = self.property("dice_texture")
+    #     texture.dispatch(self)
+    #
+    #     texture = self.property("points_texture")
+    #     texture.dispatch(self)
     pass
-
 
 # Create a class for all screens in which you can include
 # helpful methods specific to that screen
 class ScreenMenu(Screen):
+
+
     pass
 
+
+
 class ScreenGame(Screen):
+
     pass
+
 
 class ScreenHelp(Screen):
     pass
+
 
 class ScreenScores(Screen):
     pass
@@ -189,11 +195,14 @@ screen_manager.add_widget(ScreenScores(name="screen_scores"))
 
 
 class Backgammon(App):
-    # def on_start(self):
-    #     Clock.schedule_interval(self.root.ids.background.scroll_textures, 1 / 500.)
 
     def build(self):
         return screen_manager
+
+    # def on_start(self):
+    #     Clock.schedule_interval(self.root.ids.background.scroll_textures, 1 / 500.)
+    pass
+
 
 app = Backgammon()
 app.run()
