f4acfb572f5498ee91ea7e5a605bb706554ce9a3
VanessaWhite26 <46722056+VanessaWhite26@users.noreply.github.com>
2020-04-23 07:14:07 -0700

    Help Page done and combined with Rex's code

--- main.py
+++ main.py
@@ -28,25 +28,11 @@ Config.set('graphics', 'height', '750')
 Window.size = (1000, 750)
 ##----------------------------------------------------------------------------------------------------------------------
 # You can create your kv code in the Python file
-root = Builder.load_string("""
+Builder.load_string("""
 
-# 
-# Background: 
-#     id: background
-#     canvas.before:
-#         Rectangle:
-#             size: self.size
-#             pos: self.pos
-#             source: "backgroundMenu.png"
-#         Rectangle:
-#             size: self.width, 138
-#             pos: self.pos[0], self.pos[1] + self.height -138
-#             texture: self.dice_texture
-#         Rectangle:
-#             size: self.width, 500
-#             pos: self.pos[0], self.pos[1]
-#             texture: self.points_texture
 
+
+         
 
 <Button>:
     font_name: 'Arial'
@@ -61,6 +47,15 @@ root = Builder.load_string("""
 
 
 <ScreenMenu>:
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "menuBackground.png"
     
     FloatLayout:
         BoxLayout:
@@ -110,6 +105,26 @@ root = Builder.load_string("""
         
 
 <ScreenGame>:
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -400
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "backgroundMenu.png"
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -950
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.width, 500
+                    pos: self.pos[0], self.pos[1] + self.height -300
+                    texture: self.points_texture
     BoxLayout:
         Button:
             text: "Main Menu"
@@ -122,32 +137,99 @@ root = Builder.load_string("""
                 exit()
                 
 <ScreenHelp>:                          
-                 
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "backgroundMenu.png"
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -950
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.width, 500
+                    pos: self.pos[0], self.pos[1] + self.height -300
+                    texture: self.points_texture
+                
+                    
     ScrollView:
+        do_scroll_x: False
+        do_scroll_y: True
         Label: 
             halign: 'left'
             valign: 'bottom'       
-            size_hint: None, None
+            size_hint: 1, None
             size: self.texture_size
-            text: ' Game Instructions: \\n Every roll presents the opportunity to move towards pieces to the end of the board \\n Each player must roll two dice:' * 50    
-                # 'If the dice is not a 'doubles' then only two moves are allowed one for each number on the dice '
-                # If the dice roll is a 'doubles' meaning both of the dice have the same number then four moves of that number on the dice is allowed  
-                # *No player is allowed to 'pass' or not use their moves (there must be a move made every turn if possible)"
+            text:
+                '\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n'+\
+                'Game Instructions: \\n\\n' +\
+                'Every roll presents the opportunity to move towards pieces to the end of the board \\n' +\
+                'Each player must roll two dice:\\n' +\
+                'If the dice is not a "doubles" then only two moves are allowed one for each number on the dice\\n' +\ 
+                'If the dice roll is a "doubles" meaning both of the dice have the same number then four moves of that number on the dice is allowed\\n' +\
+                '*No player is allowed to "pass" or not use their moves (there must be a move made every turn if possible)\\n\\n'+\
+                'A point with two or more pieces of the same colour on it is safe - the opponent cannot land a piece on such a point\\n'+\
+                'A point hosting only one piece is called a "blot"; Such a piece is vulnerable \\n'+\
+                'If the opponent lands on this point the piece is captured and moved to the bar\\n'+\
+                'This means physically placed on the middle bar dividing the board\\n\\n'+\
+                'Captured pieces are re-entered on the furthest point from the players inner table:\\n'+\
+                'A throw of 1 allows the piece to move from the bar to point one of the opponents inner table\\n'+\
+                'A throw of 5 allows the piece to enter at point 5 of the opponents inner table\\n\\n'+\
+                'If a player has one or more pieces on the bar, no other pieces can be moved until all such pieces have re-entered play: \\n'+\
+                'So if the dice throw and position of enemy pieces prevents a player from re-entering a piece onto the board from the bar\\n'+\
+                'the player cannot move any other piece and play passes to the opponent\\n'+\
+                'A point hosting two or more of the opponents pieces is said to be "blocked"\\n'+\
+                'If six points in a row are blocked, the opponent is said to have formed a "prime"\\n'+\
+                'This is a highly advantageous achievement because a prime cannot be traversed by an opponent\\n\\n'+\
+                'Bearing off: \\n\\n' +\
+                'Once all pieces are present in a players inner table, that player can start "bearing off"\\n'+\
+                'A throw of 1 allows a player to bear off a piece from point 1 of his inner table \\n'+\
+                'A throw of 2 allows a player to bear off a piece from point 2 of his inner table and so on\\n'+\
+                'Pieces borne off are simply removed from the board\\n\\n'+\
+                'When a player rolls a number that is higher than the highest point of the inner table upon which that player has pieces\\n'+\
+                'The player is allowed to bear off the next highest piece. For example, with a roll of double 5\\n'+\
+                'If the player has a piece on point 5, two pieces on point 3, one piece on point 2 and one piece on point 1\\n'+\
+                'The player would bear off the four highest placed pieces and be left with just one piece on point 1\\n\\n'+\
+                'Winner rules: \\n\\n' +\
+                'The first player to bear off all pieces wins the game \\n' +\
+                'If the opponent has borne off at least one piece, a single game is won and the current stake is forfeited \\n' +\
+                'If the opponent has not borne off any pieces, this is a "gammon" and worth double the current stake \\n' +\
+                'If the opponent has a piece left on the bar or within the opponents inner table, this is a "backgammon" and worth triple the current stake\\n\\n'+\
+                'Keyboard Movements: \\n\\n'+\
+                '\\n'*10
+                
+                #Instructions from https://www.mastersofgames.com/rules/backgammon-rules.htm                
+                
+                    
+                
+                
     
     BoxLayout:
         Button:
             text: "Main Menu"
             on_press:
                 root.manager.transition.direction = 'left'
-                root.manager.current = 'screen_menu'            
-                
-                
+                root.manager.current = 'screen_menu'        
+
 <ScreenScores>:
+    
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "roll.png"
     BoxLayout:
         Button:
             text: "Main Menu"
             on_press:
-                root.manager.transition.direction = 'down'
+                root.manager.transition.direction = 'up'
                 root.manager.current = 'screen_menu'             
 """)
 
@@ -181,6 +263,10 @@ class Background(Widget):
 
         texture = self.property("points_texture")
         texture.dispatch(self)
+
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1 / 500.)
+
     pass
 
 ##----------------------------------------------------------------------------------------------------------------------
@@ -212,12 +298,11 @@ screen_manager.add_widget(ScreenScores(name="screen_scores"))
 ##----------------------------------------------------------------------------------------------------------------------
 
 class Backgammon(App):
-    # def on_start(self):
-    #     Clock.schedule_interval(self.root.ids.background.scroll_textures, 1 / 500.)
 
     def build(self):
         return screen_manager
 
+    pass
 
 app = Backgammon()
 app.run()
