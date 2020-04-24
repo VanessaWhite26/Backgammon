a3637f788dce51ff233b2d856900229532858265
mbierach <mbierach@uvm.edu>
2020-04-24 19:34:24 -0400

    Created GridLayout for added input. Removed master button formating and applied formatting to every button individualy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import kivy
-from kivy.uix.stacklayout import StackLayout
 
 kivy.require('1.9.0')
-
 
 from kivy.app import App
 from kivy.core.audio import SoundLoader
@@ -18,11 +16,13 @@ from kivy.base import runTouchApp
 from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 import kivy
 from kivy.clock import Clock
 from kivy.config import Config
 from kivy.core.window import Window
+
 ##----------------------------------------------------------------------------------------------------------------------
 Config.set('graphics', 'width', '1000')
 Config.set('graphics', 'height', '750')
@@ -32,20 +32,50 @@ Window.size = (1000, 750)
 Builder.load_string("""
 
 
+<MyInstructions@Label>:
+    color:1, 1 ,1,1
+    text:
+        '\\n\\n\\n\\n'+\
+        'Game Instructions: \\n\\n' +\
+        'Every roll presents the opportunity to move towards pieces to the end of the board \\n' +\
+        'Each player must roll two dice:\\n' +\
+        'If the dice is not a "doubles" then only two moves are allowed one for each number on the dice\\n' +\ 
+        'If the dice roll is a "doubles" meaning both of the dice have the same number then four moves of that number on the dice is allowed\\n' +\
+        '*No player is allowed to "pass" or not use their moves (there must be a move made every turn if possible)\\n\\n'+\
+        'A point with two or more pieces of the same colour on it is safe - the opponent cannot land a piece on such a point\\n'+\
+        'A point hosting only one piece is called a "blot"; Such a piece is vulnerable \\n'+\
+        'If the opponent lands on this point the piece is captured and moved to the bar\\n'+\
+        'This means physically placed on the middle bar dividing the board\\n\\n'+\
+        'Captured pieces are re-entered on the furthest point from the players inner table:\\n'+\
+        'A throw of 1 allows the piece to move from the bar to point one of the opponents inner table\\n'+\
+        'A throw of 5 allows the piece to enter at point 5 of the opponents inner table\\n\\n'+\
+        'If a player has one or more pieces on the bar, no other pieces can be moved until all such pieces have re-entered play: \\n'+\
+        'So if the dice throw and position of enemy pieces prevents a player from re-entering a piece onto the board from the bar\\n'+\
+        'the player cannot move any other piece and play passes to the opponent\\n'+\
+        'A point hosting two or more of the opponents pieces is said to be "blocked"\\n'+\
+        'If six points in a row are blocked, the opponent is said to have formed a "prime"\\n'+\
+        'This is a highly advantageous achievement because a prime cannot be traversed by an opponent\\n\\n'+\
+        'Bearing off: \\n\\n' +\
+        'Once all pieces are present in a players inner table, that player can start "bearing off"\\n'+\
+        'A throw of 1 allows a player to bear off a piece from point 1 of his inner table \\n'+\
+        'A throw of 2 allows a player to bear off a piece from point 2 of his inner table and so on\\n'+\
+        'Pieces borne off are simply removed from the board\\n\\n'+\
+        'When a player rolls a number that is higher than the highest point of the inner table upon which that player has pieces\\n'+\
+        'The player is allowed to bear off the next highest piece. For example, with a roll of double 5\\n'+\
+        'If the player has a piece on point 5, two pieces on point 3, one piece on point 2 and one piece on point 1\\n'+\
+        'The player would bear off the four highest placed pieces and be left with just one piece on point 1\\n\\n'+\
+        'Winner rules: \\n\\n' +\
+        'The first player to bear off all pieces wins the game \\n' +\
+        'If the opponent has borne off at least one piece, a single game is won and the current stake is forfeited \\n' +\
+        'If the opponent has not borne off any pieces, this is a "gammon" and worth double the current stake \\n' +\
+        'If the opponent has a piece left on the bar or within the opponents inner table, this is a "backgammon" and worth triple the current stake\\n\\n'+\
+        'Keyboard Movements: \\n\\n'+\
+        '\\n'*10
 
+         #Instructions from https://www.mastersofgames.com/rules/backgammon-rules.htm  
 
-         
-
-<Button>:
-    font_name: 'Arial'
-    font_size: 45
-    bold: True
-    color: 0, 1, 0, 1
-    size_hint: 0.3,0.2
-    size: self.texture_size
-    background_normal: 'leather.png'
-    background_down: 'downleather.png'
-    
+    size_hint_y:None
+    height: self.texture_size[1]
 
 
 
@@ -54,60 +84,150 @@ Builder.load_string("""
         Background: 
             id: background
             canvas.before:
-                
+
                 Rectangle:
                     size: self.size
                     pos: self.pos
                     source: "menuBackground.png"
-    
+
     FloatLayout:
         BoxLayout:
             orientation: "vertical"
             BoxLayout:
                 Button:
+                    font_name: 'Arial'
+                    font_size: 25
+                    bold: True
+                    color: 0, 1, 0, 1
+                    size_hint: 0.3,0.2
+                    background_normal: 'leather.png'
+                    background_down: 'downleather.png'
                     pos_hint:{"top": 1,"left": 1}
                     text: "Need Help?"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
                         root.manager.transition.direction = 'right'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_help'
-                        
-                
-     
+
+
+
                 Button:
+                    font_name: 'Arial'
+                    font_size: 25
+                    bold: True
+                    color: 0, 1, 0, 1
+                    size_hint: 0.3,0.2
+                    background_normal: 'leather.png'
+                    background_down: 'downleather.png'
                     pos_hint:{"top": 1,"right": 1}
                     text: "Play Backgammon?"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
                         root.manager.transition.direction = 'left'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_game'
-                    
+
                 Button:
+                    font_name: 'Arial'
+                    font_size: 25
+                    bold: True
+                    color: 0, 1, 0, 1
+                    size_hint: 0.3,0.2
+                    background_normal: 'leather.png'
+                    background_down: 'downleather.png'
                     pos_hint:{"top": 1,"right": 2}
                     text: "Scores"
-                    
+
                     on_press:
                         # You can define the duration of the change
                         # and the direction of the slide
-                        root.manager.transition.direction = 'down'
+                        root.manager.transition.direction = 'up'
                         root.manager.transition.duration = 1
                         root.manager.current = 'screen_scores'
-                
+
                 Button:
+                    font_name: 'Arial'
+                    font_size: 25
+                    bold: True
+                    color: 0, 1, 0, 1
+                    size_hint: 0.3,0.2
+                    background_normal: 'leather.png'
+                    background_down: 'downleather.png'
                     pos_hint:{"top": 1,"left": 2}
                     text: "QUIT?"
-                    
+
                     on_press:
                         exit()
-        
+
 
 <ScreenGame>:
+    FloatLayout:
+        Background: 
+            id: background
+            canvas.before:
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -400
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    source: "backgroundMenu.png"
+                Rectangle:
+                    size: self.width, 138
+                    pos: self.pos[0], self.pos[1] + self.height -950
+                    texture: self.dice_texture
+                Rectangle:
+                    size: self.width, 500
+                    pos: self.pos[0], self.pos[1] + self.height -300
+                    texture: self.points_texture
+
+
+    GridLayout:
+        cols:2
+        size_hint: 1,0.25
+        rows:2
+        Label:
+            font_name: 'Arial'
+            font_size: 25
+            bold: True
+            color: 0, 1, 0, 1
+
+            text: "Let's Play!"
+        TextInput:
+            font_name: 'Arial'
+            font_size: 25
+            bold: True
+            color: 0, 1, 0, 1
+
+            text: "User Input "
+
+        Button:
+            font_name: 'Arial'
+            font_size: 25
+            bold: True
+            color: 0, 1, 0, 1 
+            background_normal: 'leather.png'
+            background_down: 'downleather.png'
+
+            text: "Main Menu"
+
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'screen_menu'
+        Button:
+            # pos: 500,656.25
+            # size_hint: 0.5,0.125
+            text: "Quit"
+            on_press:
+                exit()
+
+<ScreenHelp>:                             
     # FloatLayout:
     #     Background: 
     #         id: background
@@ -118,151 +238,70 @@ Builder.load_string("""
     #                 source: "backgroundMenu.png"
     #             Rectangle:
     #                 size: self.width, 138
-    #                 pos: self.pos[0], self.pos[1] + self.height -1200
+    #                 pos: self.pos[0], self.pos[1] + self.height -950
     #                 texture: self.dice_texture
     #             Rectangle:
     #                 size: self.width, 500
     #                 pos: self.pos[0], self.pos[1] + self.height -300
     #                 texture: self.points_texture
-    
-    Board: 
-        id: board
-        canvas.before:
-            Rectangle:
-                size: self.size
-                pos: self.pos
-                source: "points.png"
-        
+
+
+    ScrollView:
+        GridLayout:
+            cols:1    
+            size_hint_y: None 
+            height: self.minimum_height 
+
+            MyInstructions:
+
+
+
+
+
+
+
     BoxLayout:
         Button:
-            text: "Main Menu"
-            on_press:
-                root.manager.transition.direction = 'right'
-                root.manager.current = 'screen_menu'
-        Button:
-            text: "Quit"
-            on_press:
-                exit()
-                
-<ScreenHelp>:
-    FloatLayout:
-        Background:    
-            id: background
-            canvas.before:
-                Rectangle:
-                    size: self.size
-                    pos: self.pos
-                    source: "backgroundMenu.png"     
-                Rectangle:
-                    size: self.width, 138
-                    pos: self.pos[0], self.pos[1] + self.height -138
-                    texture: self.dice_texture
-               
-                        
-                
-            
-        
-        
-        ScrollView:
-            do_scroll_x: False
-            do_scroll_y: True
-            Label: 
-                halign: 'left'
-                valign: 'bottom'       
-                size_hint: 1, None
-                size: self.texture_size
-                text:
-                    '\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n'+\
-                    'Game Instructions: \\n\\n' +\
-                    'Every roll presents the opportunity to move towards pieces to the end of the board \\n' +\
-                    'Each player must roll two dice:\\n' +\
-                    'If the dice is not a "doubles" then only two moves are allowed one for each number on the dice\\n' +\ 
-                    'If the dice roll is a "doubles" meaning both of the dice have the same number then four moves of that number on the dice is allowed\\n' +\
-                    '*No player is allowed to "pass" or not use their moves (there must be a move made every turn if possible)\\n\\n'+\
-                    'A point with two or more pieces of the same colour on it is safe - the opponent cannot land a piece on such a point\\n'+\
-                    'A point hosting only one piece is called a "blot"; Such a piece is vulnerable \\n'+\
-                    'If the opponent lands on this point the piece is captured and moved to the bar\\n'+\
-                    'This means physically placed on the middle bar dividing the board\\n\\n'+\
-                    'Captured pieces are re-entered on the furthest point from the players inner table:\\n'+\
-                    'A throw of 1 allows the piece to move from the bar to point one of the opponents inner table\\n'+\
-                    'A throw of 5 allows the piece to enter at point 5 of the opponents inner table\\n\\n'+\
-                    'If a player has one or more pieces on the bar, no other pieces can be moved until all such pieces have re-entered play: \\n'+\
-                    'So if the dice throw and position of enemy pieces prevents a player from re-entering a piece onto the board from the bar\\n'+\
-                    'the player cannot move any other piece and play passes to the opponent\\n'+\
-                    'A point hosting two or more of the opponents pieces is said to be "blocked"\\n'+\
-                    'If six points in a row are blocked, the opponent is said to have formed a "prime"\\n'+\
-                    'This is a highly advantageous achievement because a prime cannot be traversed by an opponent\\n\\n'+\
-                    'Bearing off: \\n\\n' +\
-                    'Once all pieces are present in a players inner table, that player can start "bearing off"\\n'+\
-                    'A throw of 1 allows a player to bear off a piece from point 1 of his inner table \\n'+\
-                    'A throw of 2 allows a player to bear off a piece from point 2 of his inner table and so on\\n'+\
-                    'Pieces borne off are simply removed from the board\\n\\n'+\
-                    'When a player rolls a number that is higher than the highest point of the inner table upon which that player has pieces\\n'+\
-                    'The player is allowed to bear off the next highest piece. For example, with a roll of double 5\\n'+\
-                    'If the player has a piece on point 5, two pieces on point 3, one piece on point 2 and one piece on point 1\\n'+\
-                    'The player would bear off the four highest placed pieces and be left with just one piece on point 1\\n\\n'+\
-                    'Winner rules: \\n\\n' +\
-                    'The first player to bear off all pieces wins the game \\n' +\
-                    'If the opponent has borne off at least one piece, a single game is won and the current stake is forfeited \\n' +\
-                    'If the opponent has not borne off any pieces, this is a "gammon" and worth double the current stake \\n' +\
-                    'If the opponent has a piece left on the bar or within the opponents inner table, this is a "backgammon" and worth triple the current stake\\n\\n'+\
-                    'Keyboard Movements: \\n\\n'+\
-                    '\\n'*10
-                    
-                   #Instructions from https://www.mastersofgames.com/rules/backgammon-rules.htm   
-        
-            
-                                          
-                    
-        
-     
-   
-    
-    BoxLayout:
-        Button:
+            font_name: 'Arial'
+            font_size: 25
+            bold: True                
+            color: 0, 1, 0, 1
+            size_hint: 0.3,0.2
+            background_normal: 'leather.png'
+            background_down: 'downleather.png'
             text: "Main Menu"
             on_press:
                 root.manager.transition.direction = 'left'
                 root.manager.current = 'screen_menu'        
 
 <ScreenScores>:
-    
+
     FloatLayout:
         Background: 
             id: background
             canvas.before:
-                
+
                 Rectangle:
                     size: self.size
                     pos: self.pos
                     source: "roll.png"
     BoxLayout:
         Button:
+            font_name: 'Arial'
+            font_size: 25
+            bold: True
+            color: 0, 1, 0, 1
+            size_hint: 0.3,0.2
+            background_normal: 'leather.png'
+            background_down: 'downleather.png'
             text: "Main Menu"
             on_press:
                 root.manager.transition.direction = 'up'
                 root.manager.current = 'screen_menu'             
 """)
 
+
 ##----------------------------------------------------------------------------------------------------------------------
-class Board(Widget):
-
-    board = []
-
-    def build(self):
-        # Config.set('graphics', 'width', '600')
-        # Config.set('graphics', 'height', '600')
-        self.layout = StackLayout()
-        for x in range(3):
-            bt = Button(text='', font_size=200, width=200, height=200, size_hint=(None, None), id=str(x))
-            bt.bind(on_release=self.btn_pressed)
-            self.board.append(bt)
-            self.layout.add_widget(bt)
-        return self.layout
-
-
-
-
 class Background(Widget):
     dice_texture = ObjectProperty(None)
     points_texture = ObjectProperty(None)
@@ -279,11 +318,10 @@ class Background(Widget):
         self.points_texture.wrap = 'repeat'
         self.points_texture.uvsize = (Window.width / self.dice_texture.width, -1)
 
-
     def scroll_textures(self, time_passed):
         # update the uvpos
         self.dice_texture.uvpos = (
-            (self.dice_texture.uvpos[0] + time_passed) % Window.width, self.dice_texture.uvpos[0])
+            (self.dice_texture.uvpos[0] + time_passed) % Window.width, self.dice_texture.uvpos[1])
         self.points_texture.uvpos = (
             (self.points_texture.uvpos[0] - time_passed) % Window.width, self.points_texture.uvpos[1])
         # Redraw the texture
@@ -298,20 +336,25 @@ class Background(Widget):
 
     pass
 
+
 ##----------------------------------------------------------------------------------------------------------------------
 # Create a class for all screens in which you can include
 # helpful methods specific to that screen
 class ScreenMenu(Screen):
     pass
 
+
 class ScreenGame(Screen):
     pass
+
 
 class ScreenHelp(Screen):
     pass
 
+
 class ScreenScores(Screen):
     pass
+
 
 ##----------------------------------------------------------------------------------------------------------------------
 # The ScreenManager controls moving between screens
@@ -324,6 +367,7 @@ screen_manager.add_widget(ScreenGame(name="screen_game"))
 screen_manager.add_widget(ScreenHelp(name="screen_help"))
 screen_manager.add_widget(ScreenScores(name="screen_scores"))
 
+
 ##----------------------------------------------------------------------------------------------------------------------
 
 class Backgammon(App):
@@ -332,6 +376,7 @@ class Backgammon(App):
         return screen_manager
 
     pass
+
 
 app = Backgammon()
 app.run()
